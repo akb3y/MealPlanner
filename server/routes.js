bad14e16e171fbea269ef54412aa625092e7f2db
akb3y <alisha.barraw@gmail.com>
2024-01-11 09:24:35 -0700

    added updateRecipe route

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const express = require("express");
 const pool = require("./database");
 
@@ -48,7 +49,6 @@ router.post("/addRecipe", (req, res) => {
       console.error("Error executing query", err);
       res.status(500).json({ error: "Internal Server Error" });
     } else {
-      // res.json(result.rows[0]);
       res.status(200).send("Successfully added");
     }
   });
@@ -66,6 +66,34 @@ router.delete("/removeRecipe/:recipe_id", (req, res) => {
       res.status(500).json({ error: "Internal Server Error" });
     } else {
       res.status(200).send("Successfully removed recipe");
+    }
+  });
+});
+
+router.put("/updateRecipe/:recipe_id", (req, res) => {
+  const { recipe_id } = req.params;
+  const { name, description, photos, ingredients, instructions } = req.body;
+
+  // Check if required fields are provided
+  if (!name || !description || !ingredients || !instructions) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  // Update data in PostgreSQL database
+  const query = `
+    UPDATE recipes
+    SET name = $1, description = $2, photos = $3, ingredients = $4, instructions = $5
+    WHERE recipe_id = $6
+  `;
+
+  const values = [name, description, photos, ingredients, instructions, recipe_id];
+
+  pool.query(query, values, (err, result) => {
+    if (err) {
+      console.error("Error executing query", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      res.status(200).json({ message: "Recipe updated successfully" });
     }
   });
 });
