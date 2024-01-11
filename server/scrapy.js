39d516d73bf19c9dc9c0193cfba1924de5a94c4c
akb3y <alisha.barraw@gmail.com>
2024-01-11 09:25:46 -0700

    fixed scraper

--- server/scrapy.js
+++ server/scrapy.js
@@ -7,77 +7,88 @@ const pool = require("./database");
 
 const app = express();
 
-const url = "https://www.seriouseats.com/persian-braised-eggplant-with-kashk-recipe-8424540"
-// let recipeUrls = [];
-// axios
-//   .get(url)
-//   .then((response) => {
-//     const html = response.data;
-//     const $ = cheerio.load(html);
+const url = "https://www.seriouseats.com/all-recipes-5117779";
+let recipeUrls = [];
+axios
+  .get(url)
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-//     // Replace '.recipe-link' with the appropriate selector for recipe links
-//     $(".comp").each((index, element) => {
-//       if (element && element.attribs && element.attribs.href) {
-//         recipeUrls.push(element.attribs.href);
-//       }
-//     });
+    // Replace '.recipe-link' with the appropriate selector for recipe links
+    $(".comp").each((index, element) => {
+      if (element && element.attribs && element.attribs.href) {
+        recipeUrls.push(element.attribs.href);
+      }
+    });
 
-//     console.log(recipeUrls);
-//   })
-//   .catch((error) => {
-//     console.error("Error:", error.message);
-//   });
+    // console.log("finished compiling [ ", recipeUrls.length, " ] urls...");
 
-  axios(url)
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const recipes = [];
-      let name;
-      let description;
-      let photos;
-      let ingredients = [];
-      let instructions = [];
+    scrapeUrls(recipeUrls);
+  })
+  .catch((error) => {
+    console.error("Error:", error.message);
+  });
 
-      // Assuming the data is inside a container with a specific class or ID
-      $(".article-header").each(function () {
-        name = $(this).find("h1").text();
-        description = $(this).find("p").text();
-        photos = $(this).find("img").attr("src");
-      });
+const scrapeUrls = (recipeUrls) => {
+  recipeUrls.forEach((recipeUrl, index) => {
+    // console.log("[ ", index, " ] ", recipeUrl);
+    axios(recipeUrl)
+      .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const recipes = [];
+        let name;
+        let description;
+        let photos;
+        let ingredients = [];
+        let instructions = [];
 
-      $(".primary-image__media").each(function () {
-        photos = $(this).find("img").attr("src");
-      });
+        // Assuming the data is inside a container with a specific class or ID
+        $(".article-header").each(function () {
+          const h1Element = $(this).find("h1");
 
-      $("#structured-ingredients_1-0 li").each(function () {
-        let ingredient = $(this).text().trim(); // trim to remove leading/trailing spaces
-        ingredients.push(ingredient);
-      });
+          if (h1Element) {
+            name = $(this).find("h1").text();
+            description = $(this).find("p").text();
+          }
+        });
 
-      $("#structured-project__steps_1-0 p").each(function () {
-        let instruction = $(this).text().trim();
-        instructions.push(instruction);
-      });
+        $(".primary-image__media").each(function () {
+          photos = $(this).find("img").attr("src");
+        });
 
-      recipes.push({ name, description, url, photos, ingredients, instructions });
+        $("#structured-ingredients_1-0 li").each(function () {
+          let ingredient = $(this).text().trim(); // trim to remove leading/trailing spaces
+          ingredients.push(ingredient);
+        });
 
-      // Insert data into PostgreSQL database
-      const query =
-        "INSERT INTO recipes (name, description, url, photos, ingredients, instructions) VALUES ($1, $2, $3, $4, $5, $6)";
-      const values = [name, description, url, photos, ingredients, instructions];
+        $("#structured-project__steps_1-0 p").each(function () {
+          let instruction = $(this).text().trim();
+          instructions.push(instruction);
+        });
 
-      pool.query(query, values, (err, result) => {
-        if (err) {
-          console.error("Error executing query", err);
+        if (ingredients.length > 0) {
+          recipes.push({ name, description, url: recipeUrl, photos, ingredients, instructions });
+
+          // Insert data into PostgreSQL database
+          const query =
+            "INSERT INTO recipes (name, description, url, photos, ingredients, instructions) VALUES ($1, $2, $3, $4, $5, $6)";
+          const values = [name, description, recipeUrl, photos, ingredients, instructions];
+
+          pool.query(query, values, (err, result) => {
+            if (err) {
+              console.error("Error executing query", err);
+            } else {
+              console.log("Data inserted into the database");
+            }
+          });
         } else {
-          console.log("Data inserted into the database");
+          console.log("Ingredients array is empty. Skipping insertion into the database.");
         }
-      });
-      // // Log the recipes array (optional)
-      console.log(instructions);
-    })
-    .catch((err) => console.log(err));
-
+      })
+      .catch((err) => console.log(err));
+  });
+};
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
