06d3825e5fbe9853a6d246c98e8dc16a3968ebb6
alisha barraw <alisha.barraw@gmail.com>
2024-01-22 14:08:24 -0700

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../public/style.css";
 import SideBar from "./components/SideBar";
 
 const App = () => {
   const [page, setPage] = useState("Home");
-  return <SideBar />;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3100/getAll");
+        const result = await response.json();
+
+        // Update the state with the fetched data
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    // Call the fetchData function
+    fetchData();
+  }, []);
+  return <SideBar setPage={setPage} />;
 };
 
 export default App;
